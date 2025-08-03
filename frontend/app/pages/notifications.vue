a4d4c427f7020e5d2b49df9d9a651873f4891c22
Zadigo <pendenquejohn@gmail.com>
2025-08-03 23:43:22 +0200

    Refactor notifications

--- frontend/app/pages/notifications.vue
+++ frontend/app/pages/notifications.vue
@@ -1,39 +1,46 @@
 <template>
-  <section id="notifications" class="container px-5">
-    <div class="row">
-      <div class="col-12">
-        <div class="card shadow-none">
-          <div class="card-body">
-            <div class="d-flex justify-content-center gap-2">
-              <v-btn to="/notifications" size="x-large" rounded="xl" color="primary" flat>
-                <v-badge :content="48" inline>
-                  All
-                </v-badge>
-              </v-btn>
-    
-              <v-btn to="/notifications/messages" size="x-large" rounded="xl" color="primary" flat>
-                <v-badge :content="48" inline>
-                  Messages
-                </v-badge>
-              </v-btn>
-              
-              <v-btn to="/notifications/updloads" size="x-large" rounded="xl" color="primary" flat>
-                <v-badge :content="148" inline>
-                  Uploads
-                </v-badge>
-              </v-btn>
-            </div>
-          </div>
-        </div>
-      </div>
+  <section id="notifications" class="w-4xl mx-auto">
+    <div class="py-5 flex justify-end rounded-lg mb-10">
+      <VoltSelectButton v-model="notificationType" :options="['All', 'Messages', 'Uploads']" />
+    </div>
 
-      <div class="col-sm-12 col-md-8 offset-md-2 mt-4">
-        <article v-for="i in 100" :key="i" class="card shadow-sm mb-1">
-          <div class="card-body">
-            Notification
-          </div>
-        </article>
+    <div class="space-y-2">
+      <VoltCard v-for="notification in notifications" :key="notification" class="shadow-sm">
+        <template #content>
+          <article>
+            {{ notification }}
+          </article>
+        </template>
+      </VoltCard>
+
+      <div ref="moreButtonEl" class="py-5">
+        <VoltButton @click="() => {}">
+          Load More
+        </VoltButton>
       </div>
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+const notificationType = ref<'All' | 'Messages' | 'Uploads'>('All')
+
+const notifications = ref<string[]>([])
+
+onMounted(() => {
+  notifications.value = Array.from({ length: 100 }, (_, i) => `Notification ${i + 1}`)
+  document.body.classList.add('bg-primary-600/30')
+})
+
+onUnmounted(() => {
+  document.body.classList.remove('bg-primary-600/30')
+})
+
+const moreButtonEl = useTemplateRef<HTMLElement>('moreButtonEl')
+
+useIntersectionObserver(moreButtonEl, (isIntersecting) => {
+  if (isIntersecting) {
+    notifications.value.push(...Array.from({ length: 20 }, (_, i) => `Notification ${i + 1}`))
+  }
+})
+</script>
