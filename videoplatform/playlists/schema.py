4d91c6b4c8011e51851ebaca0903e05a9ae45110
Zadigo <pendenquejohn@gmail.com>
2025-12-12 18:16:39 +0100

    Update graphql

--- videoplatform/playlists/schema.py
+++ videoplatform/playlists/schema.py
@@ -1,25 +1,37 @@
 import graphene
-from graphene_django import DjangoObjectType
-from playlists.models import Playlist
 from django.core.cache import cache
+from graphene import relay
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+from playlists.models import Playlist
 
 
 class PlaylistType(DjangoObjectType):
     class Meta:
         model = Playlist
-        fields = '__all__'
+        interfaces = (relay.Node,)
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'description': ['icontains'],
+            'visibility': ['exact'],
+            'is_intelligent': ['exact'],
+            'created_on': ['exact', 'lt', 'gt'],
+        }
 
     @classmethod
     def get_queryset(cls, queryset, info):
-        return queryset.select_related('user').prefetch_related('videos')
+        return queryset.select_related('user').prefetch_related('videos').all()
+
+
+class PlaylistConnection(relay.Connection):
+    class Meta:
+        node = PlaylistType
 
 
 class PlaylistQuery(graphene.ObjectType):
-    playlist = graphene.Field(PlaylistType, id=graphene.ID(required=True))
-    allplaylists = graphene.List(PlaylistType)
+    playlist = graphene.Node.Field(PlaylistType)
+    allplaylists = relay.ConnectionField(PlaylistConnection)
+    searchplaylists = DjangoFilterConnectionField(PlaylistType)
 
-    def resolve_playlist(self, info, id):
-        return Playlist.objects.get(pk=id)
-
-    def resolve_allplaylists(self, info):
-        return Playlist.objects.all()
+    def resolve_allplaylists(self, info, **kwargs):
+        return Playlist.objects.select_related('user').prefetch_related('videos').all()
--- videoplatform/videos/schema.py
+++ videoplatform/videos/schema.py
@@ -2,17 +2,33 @@ import graphene
 from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-from videos.models import Video
+from videos.models import Video, Tag
 
 
-class VideosNode(DjangoObjectType):
+class VideoTagType(DjangoObjectType):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class VideosType(DjangoObjectType):
     class Meta:
         model = Video
         interfaces = (relay.Node,)
         filter_fields = {
             'title': ['exact', 'icontains', 'istartswith'],
             'description': ['icontains'],
-            'video_id': ['exact']
+            'video_id': ['exact'],
+            'age_restricted': ['exact'],
+            'category': ['exact'],
+            'visibility': ['exact'],
+            'recording_date': ['exact', 'lt', 'gt'],
+            'recording_language': ['exact'],
+            'views': ['lte', 'gte'],
+            'created_on': ['exact', 'lt', 'gt'],
+            'tags__name': ['exact', 'icontains'],
+            'user__username': ['exact', 'icontains'],
+            'user_channel__name': ['exact', 'icontains'],
         }
 
     @classmethod
@@ -23,14 +39,14 @@ class VideosNode(DjangoObjectType):
 
 class VideosConnection(relay.Connection):
     class Meta:
-        node = VideosNode
+        node = VideosType
 
 
 class VideosQuery(graphene.ObjectType):
-    video = graphene.Node.Field(VideosNode)
-    # allvideos = DjangoFilterConnectionField(VideosNode)
+    video = graphene.Node.Field(VideosType)
     allvideos = relay.ConnectionField(VideosConnection)
-    searchvideos = DjangoFilterConnectionField(VideosNode)
+    searchvideos = DjangoFilterConnectionField(VideosType)
+    alltags = graphene.List(VideoTagType)
 
     def resolve_allvideos(self, info, **kwargs):
         return Video.objects.select_related('user', 'user_channel', 'channel_playlist').all()
